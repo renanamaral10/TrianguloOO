3293a77a64c357c73f4697a76b6e7ca9c949716b
renan <renan@NOTEBOOK-RENAN>
2023-04-13 20:28:23 -0300

    Add project files.

---
+++ TrianguloOO.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TrianguloOO.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 numeros\n");
+            int ladoA = int.Parse(Console.ReadLine());
+            int ladoB = int.Parse(Console.ReadLine());
+            int ladoC = int.Parse(Console.ReadLine());
+
+            Triangulos triangulo = new Triangulos(ladoA, ladoB, ladoC);
+            string tipo = triangulo.CalcularTriangulos();
+
+            Console.WriteLine(tipo);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrianguloOO.ConsoleApp/Triangulos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOO.ConsoleApp
+{
+    internal class Triangulos
+    {
+        public int ladoA;
+        public int ladoB;
+        public int ladoC;
+
+        public Triangulos(int ladoA, int ladoB, int ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public string CalcularTriangulos()
+        {
+            if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA)
+            {
+                if (ladoA == ladoB && ladoA == ladoC)
+                    return "Equilatero";
+                else
+                   if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    return "Isosceles";
+                else
+                    return "Escaleno";
+            }
+            else
+                return "Triangulo invalido!";
+    }   }
+}
